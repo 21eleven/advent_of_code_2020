02ee9c2bf8ee2d671ed7fa89d0136c52149a32fe
Noah Lidell <noahlidell@gmail.com>
2020-12-08 17:22:12 -0600

    AoC 2020 - date reformat

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,12 @@ extern crate aoc_runner_derive;
 
 extern crate nom;
 
-pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
-pub mod day5;
-pub mod day6;
-pub mod day7;
+pub mod day01;
+pub mod day02;
+pub mod day03;
+pub mod day04;
+pub mod day05;
+pub mod day06;
+pub mod day07;
 
 aoc_lib! { year = 2020 }
